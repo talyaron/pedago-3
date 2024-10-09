96cdf8a4eff54fadb873b808e7be7fed89f2927a
Ranazidan1 <halabyrana8@gmail.com>
2024-10-09 13:49:44 +0300

    rana

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,21 @@
 import './style.scss';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { setupCounter } from './counter.ts'
-import { renderExamplePage } from './view/examplePage/ExamplePage.ts'
+import { setupCounter } from './counter.ts';
+import { renderExamplePage } from './view/examplePage/ExamplePage.ts';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderExamplePage();
 
+
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
-import { renderSubjectsPage } from './view/examplePage/SubjectsPage';
+document.addEventListener('DOMContentLoaded', () => {
+   
+    document.querySelector<HTMLDivElement>('#SubjectsPage')!.innerHTML = renderSubjectsPage();
+});
+function renderSubjectsPage(): string {
+    throw new Error('Function not implemented.');
+}
 
-renderSubjectsPage();
