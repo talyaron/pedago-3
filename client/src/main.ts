cc2aa56b83c8174710c50e4814a7f41b45168a3d
silam50 <ysilam50@gmail.com>
2024-10-11 01:15:17 +0300

    My Account page

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,8 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { setupCounter } from './counter.ts'
+import { renderMyAccount } from './view/MyAccount/MyAccount.ts';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderRegister();
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderMyAccount();
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
