508c9e247da719bbb5ca301bd6906dfd05d8d2ff
Cherno <daniel.cherno9@gmail.com>
2024-10-10 17:51:46 +0300

    added documentation to heade

--- client/src/view/header/header.ts
+++ client/src/view/header/header.ts
@@ -4,7 +4,7 @@ export function renderHeader(): string
     return `
     <header>
         <input id="search-box" class="form-control" type="text" placeholder="search">
-        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"> 
     </header>
-    `
+    `//todo: add image
 }
