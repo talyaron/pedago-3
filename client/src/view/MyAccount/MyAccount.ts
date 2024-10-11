f0d656388f10069eed9b51a289506fbfaa62a725
silam50 <ysilam50@gmail.com>
2024-10-11 09:29:10 +0300

    new My Account

--- client/src/view/MyAccount/MyAccount.ts
+++ client/src/view/MyAccount/MyAccount.ts
@@ -1,28 +1,26 @@
 export function renderMyAccount(): string {
-    try {
-        return `
+  try {
+    return `
         <div class="card-container">
-  <div class="card">
-    <div class="card-header">
-      <div class="logo">Logo</div>
-    </div>
-    <div class="card-body d-flex">
-      <div class="user-image">
-        <img src="" alt="user image" />
-      </div>
-      <div class="user-info">
-        <p>Yonatan Silam</p>
-        <p>Int </p>
-        <p>ID number</p>
-      </div>
-    </div>
-  </div>
+          <div class="card">
+            <div class="card-header">
+              <div class="logo">Logo</div>
+            </div>
+            <div class="card-body d-flex">
+              <div class="user-image">
+                <img src="" alt="user image" />
+              </div>
+              <div class="user-info">
+                <p>Yonatan Silam</p>
+                <p>Int </p>
+                <p>ID number</p>
+              </div>
+          </div>
+        </div>
 
     `;
-    } catch (error) {
-        console.error(error);
-        return "";
-        
-    }
-   
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
 }
