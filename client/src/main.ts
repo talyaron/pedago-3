d566e7f2965e3822f706b8f83840fd037b14ee8f
Tal Yaron <tal.yaron@gmail.com>
2024-10-11 09:57:02 +0300

    fixed Yulia's scss

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,16 @@
-import { renderRegister } from './view/register/Register.ts';
 
 
 
-import './style.scss';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.scss'
 
-import { setupCounter } from './counter.ts'
 import { renderLessons } from './view/lessons/Lessons.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderLessons();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
 
 
