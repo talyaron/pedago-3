65a02dd997563b38b2e43be63ca975513cd2421d
Elizigi <elizigi876@gmail.com>
2024-10-13 16:45:34 +0300

    Added join zoom button

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.scss'
 
+import { renderZoomIntegration } from './view/zoomLink/zoomLinkView.ts';
 import { registerUsers } from './models/joinTables/usersCoursesModel.ts';
 import { users } from './models/users.ts';
 import { getUserCourses } from './controllers/userCoursesCont.ts';
@@ -16,13 +17,12 @@ import { renderUserCourses } from './view/userCouses/UserCoursesView.ts';
 
 registerUsers();
 
-const rana = users.find(u=>u.fullName === "Rana");
+const rana:any = users.find(u=>u.fullName === "Rana");
 console.log(rana)
 
 const userCoursers = getUserCourses(rana);
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderUserCourses(userCoursers);
-
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderUserCourses(userCoursers)+renderZoomIntegration();
 
 
 
