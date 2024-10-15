ee266040eb7ccdb401fa7654036d533b9d54392a
Tal Yaron <tal.yaron@gmail.com>
2024-10-15 18:48:36 +0300

    refactor

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,22 +6,57 @@ import { courses } from './models/entities/Course.ts';
 import { getCourseSubjects } from './controllers/courseSubjectsCont.ts';
 import { testCourseSubjects } from './models/joinTables/coursesSubjectModel.ts';
 import { renderCourseSubjects } from './view/courseSubjects/courseSubjectView.ts';
+import { renderSelectCourse } from './view/components/selectCourse/renderSelectCourse.ts';
 
 
-try
-{
+
+try {
     testCourseSubjects();
-    const course = courses.find(u=>u.name === "Geometry");
-    if (!course) throw new Error("course not found")
 
-    const courseSubjects = getCourseSubjects(course);
 
-    document.querySelector<HTMLDivElement>('#app')!.innerHTML = renderCourseSubjects(courseSubjects);
+    renderFirstPage();
 }
-catch(error)
-{
+catch (error) {
     console.error(error);
 }
 
+
+function renderFirstPage(courseId?: string) {
+    try {
+        const mainPage = document.querySelector<HTMLDivElement>('#app')
+        if (!mainPage) throw new Error("main page not found");
+
+        const course = courses.find(u => u.id === courseId);
+
+        if (!course) {
+            mainPage.innerHTML = `
+             ${renderSelectCourse(courses)}
+       `;
+        } else {
+            const courseSubjects = getCourseSubjects(course);
+
+            mainPage.innerHTML = `
+           ${renderSelectCourse(courses)}
+           ${renderCourseSubjects(course, courseSubjects)}
+           `;
+        }
+
+
+
+        const courseSelection = document.querySelector<HTMLSelectElement>('#course-selection');
+        if (!courseSelection) throw new Error("course selection not found");
+
+        courseSelection.addEventListener('change', (event: any) => {
+            console.log("change")
+            const courseId = event.target.value;
+            console.log(courseId)
+            renderFirstPage(courseId);
+        })
+    }
+    catch (error) {
+        console.error(error);
+        return ""
+    }
+}
 
 
